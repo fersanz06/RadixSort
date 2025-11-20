1aaa2ee7730fed9fbe60f52e82c26816f643e42b
fersanz_06 <luisfernandosanchezbriseno052@gmail.com>
2025-11-19 21:22:43 -0600

    Commit 4

--- RadixSort.java
+++ RadixSort.java
@@ -1,7 +1,9 @@
 public class RadixSort {
-
     public static void radixSort(int[] arr) {
+        if (arr == null || arr.length < 2) return;
+
         int max = getMax(arr);
+
         for (int exp = 1; max / exp > 0; exp *= 10) {
             countingSort(arr, exp);
         }
@@ -18,27 +20,36 @@ public class RadixSort {
         int[] output = new int[arr.length];
         int[] count = new int[10];
 
-        for (int i = 0; i < arr.length; i++) {
-            int index = (arr[i] / exp) % 10;
+        for (int value : arr) {
+            int index = (value / exp) % 10;
             count[index]++;
         }
 
         for (int i = 1; i < 10; i++)
             count[i] += count[i - 1];
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = arr.length - 1; i >= 0; i--) {
             int index = (arr[i] / exp) % 10;
             output[count[index] - 1] = arr[i];
             count[index]--;
         }
 
-
-        for (int i = 0; i < arr.length; i++)
-            arr[i] = output[i];
+        System.arraycopy(output, 0, arr, 0, arr.length);
     }
 
     public static void main(String[] args) {
         int[] data = {170, 45, 75, 90, 802, 24, 2, 66};
+
+        System.out.println("Antes:");
+        printArray(data);
+
         radixSort(data);
-        for (int n : data) System.out.print(n + " ");
+
+        System.out.println("\nDespués (orden ascendente):");
+        printArray(data);
+    }
+
+    private static void printArray(int[] arr) {
+        for (int v : arr) System.out.print(v + " ");
+        System.out.println();
     }
 }
